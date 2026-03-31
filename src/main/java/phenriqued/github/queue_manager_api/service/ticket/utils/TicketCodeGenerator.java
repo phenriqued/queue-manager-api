fbfbd4c7a5cd943ce3da93cb5e4d4ca5d15b2168
phenriqued <112258290+phenriqued@users.noreply.github.com>
2026-03-31 18:11:13 -0300

    (Bug fix: Change the generated code for Ticket): correcao do bug: alterando o código de sequencia numerica para cada fila individual

--- src/main/java/phenriqued/github/queue_manager_api/service/ticket/utils/TicketCodeGenerator.java
+++ src/main/java/phenriqued/github/queue_manager_api/service/ticket/utils/TicketCodeGenerator.java
@@ -1,23 +1,25 @@
 package phenriqued.github.queue_manager_api.service.ticket.utils;
 
-import org.springframework.stereotype.Component;
-
 import java.util.concurrent.atomic.AtomicInteger;
 
-@Component
+
 public class TicketCodeGenerator {
 
+    private Long queueId;
     private final AtomicInteger normalCounter = new AtomicInteger(0);
     private final AtomicInteger priorityCounter = new AtomicInteger(0);
 
-    public String generateNextCode(Boolean isPriority){
+    public TicketCodeGenerator(Long queueId) {
+        this.queueId = queueId;
+    }
 
+    public String generateNextCode(Boolean isPriority){
         int number = isPriority ?
                         priorityCounter.incrementAndGet() :
                         normalCounter.incrementAndGet();
 
         String prefix = isPriority ? "P" : "N";
-        return String.format("%s-%04d", prefix, number);
+        return String.format("%s-%d%04d", prefix, this.queueId, number);
     }
 
 
